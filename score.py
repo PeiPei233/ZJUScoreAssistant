d2c1c5ecd55b96f6f00322d400efb6b3e735a209
PeiPei233 <2503585731@qq.com>
2022-06-23 19:32:38 +0800

    dingtalk massage fixed

--- score.py
+++ score.py
@@ -120,12 +120,12 @@ def updatescore():
                     "title": "考试成绩通知",
                     "text": "\
 ### 考试成绩通知\n\
-- **选课课号**\t%s\n\
-- **课程名称**\t%s\n\
-- **成绩**\t%s\n\
-- **学分**\t%s\n\
-- **绩点**\t%s\n\
-- **成绩变化**\t%.2f(%+.2f) / %.1f(%+.1f)" % (id, name, score, credit, gp, newgpa, newgpa - gpa, newtotcredits, newtotcredits - totcredits)
+ - **选课课号**\t%s\n\
+ - **课程名称**\t%s\n\
+ - **成绩**\t%s\n\
+ - **学分**\t%s\n\
+ - **绩点**\t%s\n\
+ - **成绩变化**\t%.2f(%+.2f) / %.1f(%+.1f)" % (id, name, score, credit, gp, newgpa, newgpa - gpa, newtotcredits, newtotcredits - totcredits)
                 }
             })
         print('考试成绩通知\n选课课号\t%s\n课程名称\t%s\n成绩\t%s\n学分\t%s\n绩点\t%s\n成绩变化\t%.2f(%+.2f) / %.1f(%+.1f)' % (id, name, score, credit, gp, newgpa, newgpa - gpa, newtotcredits, newtotcredits - totcredits))
